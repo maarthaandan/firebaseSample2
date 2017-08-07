e1e05cffa434942f13e5357108bcce7e6994c101
maarthaandan <manumaheshwar.p@gmail.com>
2017-08-07 21:17:03 +0530

    The first commit

---
+++ app/src/main/java/com/example/sample2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sample2;
+
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.R.attr.tag;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth ) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                   // Log.d(Tag,"onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    //Log.d(Tag, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
